8429aff9abdb4f92d4897be8fb30ea4d28076aec
Sanya Midha <sanyamidha.10@gmail.com>
2023-02-09 22:45:48 +0530

    Binary search questions

---
+++ BinarySearch/first_and_last_occurrences.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int firstOccur(int arr[], int n, int ele){
+    int s = 0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+    int ans =-1;
+    while(s<=e){
+        if(ele==arr[mid]){
+            ans=mid;
+            e=mid-1;
+        }
+        else if(ele>arr[mid]){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+   
+}
+
+int lastOccur(int arr[], int n, int ele){
+    int s = 0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+    int ans =-1;
+    while(s<=e){
+        if(ele==arr[mid]){
+            ans=mid;
+            s=mid+1;
+        }
+        else if(ele>arr[mid]){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+   
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter size of array: "<<endl;
+    cin>>n;
+
+    int arr[100];
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Enter the element of which you want to find first and last occurrence"<<endl;
+    int element;
+    cin>>element;
+
+    int ans1 = firstOccur(arr, n, element);
+    cout<<"First Occurrence of "<<element<<" is at index "<<ans1<<endl;
+
+    int ans2 = lastOccur(arr, n, element);
+    cout<<"Last Occurrence of "<<element<<" is at index "<<ans2<<endl;
+
+}
